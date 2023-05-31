e85680dcb4be2072b7fe7ef5dde48c495d8bfcb0
holmes <c.holmes4@gmail.com>
2023-05-31 09:11:06 +0100

    Update hex logo

--- data-raw/scanline-hex/make-logo.R
+++ data-raw/scanline-hex/make-logo.R
@@ -1,8 +1,8 @@
 scanline::scanline(
     'data-raw/scanline-hex/g34022.png', 
-    add_noise = TRUE, 
+    add_noise = FALSE, 
     border_size = 0,
     frame_size = 0,
-    n_scanlines = 30)
+    n_scanlines = 20)
 
 ggplot2::ggsave('data-raw/scanline-hex/text.pdf', width = 8, height = 3)
